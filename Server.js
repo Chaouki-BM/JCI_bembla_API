2daef91317e5e0baee9fb494272c84c95c9fbf53
Chaouki-BM <Chaouki.BenMiled@gmail.com>
2023-06-29 14:35:55 +0100

    update user controller

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const express =require('express')
 const mongoose= require('mongoose')
 const app =express()
+const userrouter=require('./Routes/user.routes')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
@@ -11,7 +12,7 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 }).catch((e) => {
     console.log('unable to connect to database ')
 })
-
+app.use('/',userrouter);
 const port = process.env.PORT 
 app.listen(port, () => {
     console.log('app is running')
--- models/user.model.js
+++ models/user.model.js
@@ -2,9 +2,9 @@ const mongoose=require('mongoose')
 const UserSchema=new mongoose.Schema({
     Email:{type:String,required:true},
     Password:{type:String,required:true},
-    FullName:{type:String,required:true},
-    DateN:{type:Date,required:true},
-    DateI:{type:Date,required:true},
+    FullName:{type:String,required:false},
+    DateN:{type:Date,required:false},
+    DateI:{type:Date,required:false},
     cin:{type:String,required:false},
     role:{type:String,required:true}
 
